a7f90645ee6c7a260ee78c7a14790362a9d76555
ACTOMtoolbox <120719451+ACTOMtoolbox@users.noreply.github.com>
2023-02-01 11:06:30 +0000

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,16 @@
+@ for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do @ set NetworkIP=%%a
+@ set Disp="%NetworkIP%:0.0"
+
+@ set TIMESTAMP=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+@ set TIMESTAMP=%TIMESTAMP: =0%
+@ set TIMESTAMP=%TIMESTAMP:_=%
+@ set title=Run-%TIMESTAMP%
+
+@ mkdir %title%
+@ cd %title%
+
+docker run -i -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=%Disp% --mount type=bind,source=%cd%,target=/srv/actom-run/input actomtoolbox/actom-run
+
+@ DEL Run-All.sh
+@ ./Run-All.bat
+@ DEL Run-All.bat
